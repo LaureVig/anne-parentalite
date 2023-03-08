8d976a7ecbfa015d9760c3ee810767d9937eb76c
Laure Vigouroux <laure.vigouroux@imt-atlantique.net>
2023-03-08 20:57:23 +0100

    Plein de trucs

---
+++ src/app/services.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Prestation } from './prestation';
+import { PRESTATIONS} from './textes-prestations';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServicesService {
+  public prestations: Prestation[] = PRESTATIONS;
+  constructor() { }
+
+  getPrestationById(prestationId: number): Prestation {
+    const prestation = this.prestations.find(prestation => prestation.id === prestationId);
+    if (!prestation) {
+        throw new Error('Pas de prestation');
+    } else {
+        return prestation;
+    }
+  }
+
+  getAllPrestations():Prestation[] {
+    return this.prestations;
+  }
+}
--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
 import { Prestation } from '../prestation';
 import { PRESTATIONS} from '../textes-prestations';
-
+import { ServicesService } from '../services.service';
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
   styleUrls: ['./services.component.scss']
 })
 export class ServicesComponent {
-  public prestations!: Prestation[]
-
-  ngOnInit():void{
-    this.prestations = PRESTATIONS;
+  public prestations !: Prestation[];
+  ngOnInit(): void {
+    this.prestations = this.servicesService.prestations;
   }
+  constructor(private servicesService: ServicesService) { }
 }
