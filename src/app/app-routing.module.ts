1d449931d207aab83a2ecbf1674468e47e5db8bb
pierre <vigouroux001@gmail.com>
2023-11-13 10:53:29 +0100

    retirer firebase

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,7 +15,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    initialNavigation: 'enabledBlocking'
+    initialNavigation: 'enabledNonBlocking'
 })],
   exports: [RouterModule]
 })
